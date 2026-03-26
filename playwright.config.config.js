8e703a2ea08a725f51939b4b02385e9b0bf456a4
nanhidev <nanhi.kumari@walkingtree.tech>
2026-03-26 16:04:52 +0530

    add config files

--- playwright.config.config.js
+++ playwright.config.config.js
@@ -5,12 +5,20 @@ module.exports = defineConfig({
   testDir: './tests',
   timeout: 30000,
 
+  retries: 1, // retry failed tests
+
   use: {
     headless: false,
     viewport: { width: 1280, height: 720 },
+
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
+    trace: 'retain-on-failure', // 🔥 very important
   },
 
-  reporter: [['html', { open: 'never' }]],
+  reporter: [
+    ['list'], // console output
+    ['html', { open: 'never' }], // HTML report
+    ['json', { outputFile: 'test-results/results.json' }], // for API usage
+  ],
 });
